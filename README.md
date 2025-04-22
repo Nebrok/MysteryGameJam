2c8c83e1e5f9c56b1614afe3d29fb34e4c7ce369
Nebrok <nebrok.red@outlook.com>
2025-04-22 11:59:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MysteryGameJam
+
+Developed with Unreal Engine 5
---
+++ Source/MysteryGameJam/MysteryGameJamGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MysteryGameJamGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMysteryGameJamGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMysteryGameJamGameMode();
+};
+
+
+
