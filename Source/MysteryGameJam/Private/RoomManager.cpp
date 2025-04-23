f7c808463f2e6bc6255305693909889966996a18
Nebrok <nebrok.red@outlook.com>
2025-04-23 09:51:08 +0200

    Created World Builder, SpawnRoom working

---
+++ Source/MysteryGameJam/Private/RoomManager.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RoomManager.h"
+#include "Room.h"
+
+void URoomManager::SpawnNewRooms(ARoom* callingRoom)
+{
+	bool movedForward = callingRoom == ForwardRoom;
+	
+	CurrentRoom->Destroy();
+	CurrentRoom = callingRoom;
+
+	SpawnNewRoom(CurrentRoom->BaseRoom, GetBackRoomLocation(), CurrentRoom->GetActorForwardVector().BackwardVector.ToOrientationRotator());
+}
+
+FVector URoomManager::GetBackRoomLocation()
+{
+	FVector location;
+	location = CurrentRoom->GetActorLocation() + (CurrentRoom->GetActorForwardVector() * -700);
+	return location;
+}
+
+AActor* URoomManager::SpawnNewRoom(UClass* baseRoom, FVector location, FRotator rotation)
+{
+	FActorSpawnParameters spawnParams;
+	spawnParams.bNoFail = true;
+
+
+	AActor* newRoom = GetWorld()->SpawnActor<ARoom>(baseRoom, location, rotation, spawnParams);
+	return newRoom;
+}
---
+++ Source/MysteryGameJam/Private/WorldBuilder.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldBuilder.h"
+
+// Sets default values
+AWorldBuilder::AWorldBuilder()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AWorldBuilder::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWorldBuilder::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
